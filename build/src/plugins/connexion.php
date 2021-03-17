65a56f7196166ac92ae9d4e8ecc569aa66cfa871
betsch <victor.betch@lyceefulbert.fr>
2021-03-17 16:51:13 +0100

    connexion secure 17.03

--- build/src/plugins/connexion.php
+++ build/src/plugins/connexion.php
@@ -3,7 +3,7 @@
     $pass = file_get_contents('../../keys/app.key');
 
     try {
-        $connexion = new PDO('mysql:host=127.0.0.1:3308;dbname=ass', $user, $pass);
+        $connexion = new PDO('mysql:host=127.0.0.1;dbname=ass', $user, $pass);
     } catch (Exception $e) {
         echo "ERREUR:<br/>".$e->getMessage();
     }
