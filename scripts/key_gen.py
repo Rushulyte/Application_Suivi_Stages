b9c73f8cef758d3ed73ec74f2d0e69c37c892294
Edhyjox <53050011+Edhyjox@users.noreply.github.com>
2021-03-17 15:37:35 +0100

    key_gen

--- scripts/key_gen.py
+++ scripts/key_gen.py
@@ -1,9 +1,23 @@
 import random
 import string
 
-with open("../keys/master_gen.key") as f:
-    master_key_gen = f.read()
-random.seed(f"{master_key_gen}{input('user : ')}")
 
-base = ''.join((string.ascii_letters, string.digits, '-_'))
-print(''.join(random.choice(base) for _ in range(32)))
+class App:
+
+    def __init__(self):
+        self.is_running = False
+        with open("../keys/master_gen.key") as f:
+            self.master_key_gen = f.read()
+
+    def __call__(self):
+        is_running = True
+
+        while is_running:
+            random.seed(f"{self.master_key_gen}{input('user : ')}")
+
+            base = ''.join((string.ascii_letters, string.digits, '-_'))
+            print(''.join(random.choice(base) for _ in range(32)))
+
+
+if __name__ == '__main__':
+    App()()
