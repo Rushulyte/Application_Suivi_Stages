386c10d56a32ed77f5ba4c7ba02f81381f7546a0
betsch <vic.betsch@gmail.com>
2021-05-12 16:57:22 +0200

    crud entreprises update

--- app/src/pages/partenaires.php
+++ app/src/pages/partenaires.php
@@ -1,6 +1,4 @@
 <?php
-#define('TEMPLATES_', dirname(__FILE__) . '../templates');
-
 session_start();
 
 if (empty($_SESSION)) {
@@ -28,23 +26,27 @@ if (empty($_SESSION)) {
 </header>
 <main>
     <h1>Nos Partenaires</h1>
-    <form action="#" method="GET">
-        <input type="submit" name="action" value="Ajouter">
-        <input type="submit" name="action" value="Modifier">
-        <input type="submit" name="action" value="Supprimer">
-    </form>
-    <table>
-        <tr>
-            <th>Nom</th>
-            <th>Contact</th>
-            <th>Mail</th>
-            <th>Tel</th>
-            <th>Adresse</th>
-        </tr>
 
-        <tbody>
-        <?php
-        $get_companies = 'select E.name as nom, 
+    <?php
+    if ($_SESSION['type'] == 'admin' or $_SESSION['type'] == 'prof') {
+        echo '<form action="#" method="GET">
+                <input type="submit" name="action" value="Ajouter">
+                <input type="submit" name="action" value="Modifier">
+                <input type="submit" name="action" value="Supprimer">
+            </form>';
+    }
+
+    echo '<table>
+            <tr>
+                <th>Nom</th>
+                <th>Contact</th>
+                <th>Mail</th>
+                <th>Tel</th>
+                <th>Adresse</th>
+            </tr>';
+
+
+    $get_companies = 'select E.name as nom, 
                                  E.contact as contact,
                                  E.mail as mail,
                                  E.tel as tel,
@@ -52,71 +54,61 @@ if (empty($_SESSION)) {
                           from entreprises E
                           order by E.name';
 
-        include '../plugins/connexion.php';
+    include '../plugins/connexion.php';
 
-        $cursor = $connexion->prepare($get_companies);
-        $cursor->execute();
+    $cursor = $connexion->prepare($get_companies);
+    $cursor->execute();
 
-        foreach ($cursor->fetchAll(PDO::FETCH_ASSOC) as $table_row) {
-            echo '<tr>';
-            foreach ($table_row as $value) {
-                echo "<td>$value</td>";
-            }
-            echo '</tr>';
+    foreach ($cursor->fetchAll(PDO::FETCH_ASSOC) as $table_row) {
+        echo '<tr>';
+        foreach ($table_row as $value) {
+            echo "<td>$value</td>";
         }
-        ?>
-        </tbody>
-    </table>
-    <?php
-
-    if (empty($_GET)) {
-        die();
+        echo '</tr>';
     }
 
-    if (!isset($_GET['action'])) {
-        die();
-    } ?>
+    if ($_SESSION['type'] == 'admin' or $_SESSION['type'] == 'prof') {
+        echo '</tbody></table><form action="#" method="GET">
+                    <input type="submit" name="action" value="Annuler">
+                  </form>';
 
-    <form action="#" method="GET">
-        <input type="submit" name="action" value="Annuler">
-    </form>
-
-    <?php
-    if ($_GET['action'] == 'Annuler') {
-        header('Location: partenaires.php');
-        die();
-    }
-
-    switch ($_GET['action']) {
-    case 'Ajouter' : ?>
-    <form action='crud_companies.php?action=Ajouter' method="POST"><?php
-        break;
-        case 'Modifier' : ?>
-        <form action='crud_companies.php?action=Modifier' method="POST"><?php
-            break;
-            case 'Supprimer' : ?>
-            <form action='crud_companies.php?action=Supprimer' method="POST"><?php
+        switch ($_GET['action']) {
+            case 'Annuler' :
+                header('Location: partenaires.php');
+                die();
                 break;
-                }
-                ?>
-                <label>
-                    <input type="text" name="nom" placeholder="Nom" required>
-                </label>
-                <label>
-                    <input type="text" name="contact" placeholder="Contact">
-                </label>
-                <label>
-                    <input type="text" name="mail" placeholder="Mail">
-                </label>
-                <label>
-                    <input type="text" name="tel" placeholder="Tel">
-                </label>
-                <label>
-                    <input type="text" name="adresse" placeholder="Adresse">
-                </label>
-                <input type="submit" name="validation" value="Valider">
-                <input type="reset" name="validation" value="Annuler">
-            </form>
+            case 'Ajouter' :
+                echo '<form action="crud_companies.php?action=Ajouter" method="POST">';
+                break;
+            case 'Modifier' :
+                echo '<form action="crud_companies.php?action=Modifier" method="POST">';
+                break;
+            case 'Supprimer' :
+                echo '<form action="crud_companies.php?action=Supprimer" method="POST">';
+                break;
+            default:
+                die();
+        }
+
+        echo '<label>
+                        <input type="text" name="nom" placeholder="Nom" required>
+                    </label>
+                    <label>
+                        <input type="text" name="contact" placeholder="Contact">
+                    </label>
+                    <label>
+                        <input type="text" name="mail" placeholder="Mail">
+                    </label>
+                    <label>
+                        <input type="text" name="tel" placeholder="Tel">
+                    </label>
+                    <label>
+                        <input type="text" name="adresse" placeholder="Adresse">
+                    </label>
+                    <input type="submit" name="validation" value="Valider">
+                    <input type="reset" name="validation" value="Annuler">
+                    </tbody>';
+    } ?>
 </main>
 </body>
 </html>
